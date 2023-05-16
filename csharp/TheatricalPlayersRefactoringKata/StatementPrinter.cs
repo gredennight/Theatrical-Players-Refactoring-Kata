5d88aafe6efe4b78e0724b615585d38c036a74a5
Greden Night <greden.ua@gmail.com>
2023-05-16 18:15:49 +0300

    Refactored StatementPrinter class. Now supports 2 new genres of performances and does not affect work with other classes in any way, thus providing support for legacy code

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,6 +6,92 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        private enum PerfomanceTypes
+        {
+            tragedy,
+            comedy,
+            history,
+            pastoral
+        }
+        private PerfomanceTypes GetType(string type)
+        {
+            switch (type)
+            {
+                case "tragedy":
+                    return PerfomanceTypes.tragedy;
+                case "comedy":
+                    return PerfomanceTypes.comedy;
+                case "history":
+                    return PerfomanceTypes.history;
+                case "pastoral":
+                    return PerfomanceTypes.pastoral;
+                default:
+                    throw new Exception("unknown type: " + type);
+            }
+        }
+        private int TragedyCase(Performance perf, Play play)
+        {
+            int thisAmount = 40000;
+            if (perf.Audience > 30)
+            {
+                thisAmount += 1000 * (perf.Audience - 30);
+            }
+            return thisAmount;
+        }
+        private int ComedyCase(Performance perf, Play play)
+        {
+            int thisAmount = 30000;
+            if (perf.Audience > 20)
+            {
+                thisAmount += 10000 + 500 * (perf.Audience - 20);
+            }
+            thisAmount += 300 * perf.Audience;
+            return thisAmount;
+        }
+        private int HistoryCase(Performance perf, Play play)
+        {
+            int thisAmount = 30000;
+            if (perf.Audience > 20)
+            {
+                thisAmount += 10000 + 500 * (perf.Audience - 20);
+            }
+            thisAmount += 300 * perf.Audience;
+            return thisAmount;
+        }
+        private int PastoralCase(Performance perf, Play play)
+        {
+            int thisAmount = 30000;
+            if (perf.Audience > 20)
+            {
+                thisAmount += 10000 + 500 * (perf.Audience - 20);
+            }
+            thisAmount += 300 * perf.Audience;
+            return thisAmount;
+        }
+        private int CalculateAmount(Performance perf, Play play)
+        {
+            switch (GetType(play.Type))
+            {
+                case PerfomanceTypes.tragedy:
+                    return TragedyCase(perf, play);
+                case PerfomanceTypes.comedy:
+                    return ComedyCase(perf, play);
+                case PerfomanceTypes.history:
+                    return HistoryCase(perf, play);
+                case PerfomanceTypes.pastoral:
+                    return PastoralCase(perf, play);
+            }
+            return 0;
+        }
+        private int CalculateVolumeCredits(Performance perf, Play play)
+        {
+            // add volume credits
+            int volumeCredits = Math.Max(perf.Audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+            return volumeCredits;
+
+        }
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
@@ -13,33 +99,11 @@ namespace TheatricalPlayersRefactoringKata
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach (var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
-
+                var thisAmount = CalculateAmount(perf, play);
+                volumeCredits += CalculateVolumeCredits(perf, play);
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
